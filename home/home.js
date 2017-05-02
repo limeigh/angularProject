130364894e810e45259751dfb625eab22b42d78a
dinghq <dinghq@kingnet.sh>
2017-05-02 18:20:55 +0800

    moviecat-home

---
+++ home/home.js
@@ -0,0 +1,11 @@
+(function(angular){
+	var app=angular.module('home',['ngRoute'])
+	app.config(['$routeProvider','$locationProvider',function($routeProvider,$locationProvider){
+		$routeProvider.when('/home_page',{
+			templateUrl:'home/home.html',
+			controller:'homeController'
+		})
+		$locationProvider.hashPrefix('')
+	}])
+	app.controller('homeController',['$scope',function($scope){}])
+})(angular)
